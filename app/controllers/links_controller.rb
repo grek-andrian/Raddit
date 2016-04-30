2aa8043c84dc8895837b5f419616fb2ce5c69541
grek-andrian <gretsko.andrian@gmail.com>
2016-04-30 23:34:58 +0300

    Add acts_as_votable

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :set_link, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
   before_filter :authenticate_user!, except: [:index, :show]
 
   # GET /links
@@ -61,6 +61,16 @@ class LinksController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+def upvote
+  @link.upvote_by current_user
+  redirect_to :back
+end
+
+def downvote
+  @link.downvote_by current_user
+  redirect_to :back
+end
 
   private
     # Use callbacks to share common setup or constraints between actions.
